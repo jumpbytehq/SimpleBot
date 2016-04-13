47bccdda8b62c954e587295f9998177273ad9f90
Dhaval Nagar <dhaval.b.nagar@gmail.com>
2016-04-13 19:57:45 +0530

    committed by parse-cli at: 2016-04-13 19:57:45.421832939 +0530 IST

--- server.js
+++ server.js
@@ -12,11 +12,23 @@ require('./cloud/main.js');
 // Mount the webhooks app to a specific path (must match what is used in scripts/register-webhooks.js)
 //app.use('/webhooks', ParseCloud.app);
 
-app.get('/webhook/', function(req, res) {  
-	if (req.query['hub.verify_token'] === '123456' ) {
-		res.send(req.query['hub.challenge']);
+app.post('/webhook/', function(req, res) {  
+	// if (req.query['hub.verify_token'] === '123456' ) {
+	// 	res.send(req.query['hub.challenge']);
+	// }
+	// res.send('Error, wrong validation token');  
+
+	messaging_events = req.body.entry[0].messaging;
+	for (i = 0; i < messaging_events.length; i++) {
+		event = req.body.entry[0].messaging[i];
+		sender = event.sender.id;
+		if (event.message && event.message.text) {
+			text = event.message.text;
+			// Handle a text message from this sender
+		}
 	}
-	res.send('Error, wrong validation token');  
+
+	res.sendStatus(200);
 });
 
 // Host static files from public/
