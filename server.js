772829bbbfa43c22e615bf61ced4d49235e27354
Dhaval Nagar <dhaval.b.nagar@gmail.com>
2016-04-13 19:51:57 +0530

    committed by parse-cli at: 2016-04-13 19:51:57.214389948 +0530 IST

--- server.js
+++ server.js
@@ -1,30 +1,11 @@
-// Require Node Modules
-var http = require('http'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    Parse = require('parse/node'),
-    ParseCloud = require('parse-cloud-express');
+express = require('express');
+app = express();
 
-var app = express();
-
-// Import your cloud code (which configures the routes)
-require('./cloud/main.js');
-// Mount the webhooks app to a specific path (must match what is used in scripts/register-webhooks.js)
-app.use('/webhooks', ParseCloud.app);
-
-// Host static files from public/
-app.use(express.static(__dirname + '/public'));
-
-// Catch all unknown routes.
-app.all('/', function(request, response) {
-  response.status(404).send('Page not found.');
-});
-/*
- * Launch the HTTP server
- */
-var port = process.env.PORT || 5000;
-var server = http.createServer(app);
-server.listen(port, function() {
-  console.log('Cloud Code Webhooks server running on port ' + port + '.');
+app.get('/webhook/', function(req, res) {  
+	if (req.query['hub.verify_token'] === '123456' ) {
+		res.send(req.query['hub.challenge']);
+	}
+	res.send('Error, wrong validation token');  
 });
 
+app.listen();
