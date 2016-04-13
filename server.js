b2be946a57069cf12c11ac37b1d270e0dd8cb45a
Dhaval Nagar <dhaval.b.nagar@gmail.com>
2016-04-13 20:05:43 +0530

    committed by parse-cli at: 2016-04-13 20:05:43.080146959 +0530 IST

--- server.js
+++ server.js
@@ -27,11 +27,35 @@ app.post('/webhook/', function(req, res) {
 		if (event.message && event.message.text) {
 			text = event.message.text;
 			// Handle a text message from this sender
+			sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
 		}
 	}
 
 	res.sendStatus(200);
 });
+
+var token = "EAAHbWFzW5fQBAKRxEzRncioaZCiZAQNl1WdSHiWhZC1jdypJ3IEgw3cQ6pH9ZCJsmwbZB7uWTL0VtDS3gJpcjF8VgegMrRNAYZCMg6m3odRf1fxmXqfmhv92ZBPwiBqaEqIlOH2m7srEX3ytLLgkZAq3nMvK4s2CoOtprOrvmXFNgAZDZD";
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 
 // Host static files from public/
 app.use(express.static(__dirname + '/public'));
